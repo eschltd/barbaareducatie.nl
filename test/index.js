a1e77e902e91bfb92041533ea3c330d94fcec220
Matt-Esch <matt@mattesch.info>
2014-03-25 06:16:36 +0000

    remove test directory

--- test/index.js
+++
@@ -1,8 +0,0 @@
-var test = require("tape")
-
-var barbaareducatieNl = require("../index")
-
-test("barbaareducatieNl is a function", function (assert) {
-    assert.equal(typeof barbaareducatieNl, "function")
-    assert.end()
-})
